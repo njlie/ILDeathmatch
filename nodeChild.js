6936fb6ca92f73438e7ad5455a43989d688d82ff
njlie <lie4nathan@gmail.com>
2018-04-30 14:01:53 -0700

    feat: server as child process

--- nodeChild.js
+++ nodeChild.js
@@ -1,7 +1,20 @@
 const { spawn } = require('child_process')
+const fetch = require('node-fetch')
 var StreamSplitter = require('stream-splitter')
+const WebSocket = require('ws')
 const quake = spawn('node', ['build/ioq3ded.js', '+set', 'fs_game', 'baseq3', '+set', 'dedicated', '2', '+exec', 'server.cfg'])
 var cmdArray = []
+var prevToken = ''
+
+const ws = new WebSocket('ws://localhost:8080/server')
+
+ws.on('open', function open() {
+  ws.send('server ws connected.')
+})
+
+ws.on('message', (message) => {
+  console.log(message)
+})
 
 var test = quake.stderr.pipe(StreamSplitter('\n'))
 test.encoding = 'utf-8'
@@ -11,15 +24,15 @@ test.on('token', token => {
   if (token.includes('ClientBegin:')) {
     const client = token.split(':').map(e => e.trim())[1]
     const dumpuser = 'dumpuser ' + client + '\n'
-    cmdArray.push('ip')
-    cmdArray.push('connect')
+    cmdArray.push({ name: 'ip', index: client })
+    // cmdArray.push({ name: 'connect' })
     quake.stdin.write(dumpuser)
   }
 
   if (token.includes('ClientDisconnect:')) {
     const client = token.split(':').map(e => e.trim())[1]
     const dumpuser = 'dumpuser ' + client + '\n'
-    cmdArray.push('disconnect')
+    // cmdArray.push('disconnect')
     quake.stdin.write(dumpuser)
   }
 
@@ -28,45 +41,48 @@ test.on('token', token => {
     const killer = players[1]
     const killed = players[2]
     console.log(`Player ${killer} killed Player ${killed}`)
-    cmdArray.push('killer')
-    cmdArray.push('killed')
+    cmdArray.push({ name: 'killer' })
+    cmdArray.push({ name: 'killed', index: killed })
     const dumpKiller = 'dumpuser ' + killer + '\n'
     const dumpKilled = 'dumpuser ' + killed + '\n'
     quake.stdin.write(dumpKiller)
     quake.stdin.write(dumpKilled)
   }
 
-  if (token.includes('ip        ')) {
+  if (token.includes('ip        ') && prevToken === '--------') {
     console.log('IP TOKEN: ', token)
-  }
-
-  if (token.includes('cl_guid')) {
-    const guid = token.split(' ').map(e => e.trim()).filter(e => e !== '')[1]
+    const ip = token.split('ip').map(e => e.trim())[1].replace(/\./g, '-')
     const action = cmdArray.shift()
-    switch (action) {
-      case 'connect': {
-        console.log(`Payment pointer is $${guid}.com`)
-        // TODO: API call to add someone to web monetization server - use IP against websockets
-        break
-      }
-      case 'killer': {
-        console.log(`Player ${guid} got a kill and is to be rewarded.`)
-        // TODO: API call to pay player.
-        break
-      }
-      case 'killed': {
-        console.log(`Player ${guid} got killed and will be punished.`)
-        // TODO: API call to charge player.
-        break
-      }
-      case 'disconnect': {
-        console.log(`Player ${guid} disconnected. Deducting balance.`)
-        // TODO: Should deduction occur here or on join?
-        break
-      }
-      default: {
-        break
-      }
+    console.log('ACTION: ', action)
+    if (action.name === 'ip') {
+      console.log('Got IP: ', ip)
+      fetch('http://localhost:8080/game/spawn/' + ip).then(res => {
+        console.log(res.status)
+        if (res.status !== 200) {
+          console.log('kick player for having no balance')
+          const kickPlayer = 'kick ' + action.index + '\n'
+          quake.stdin.write(kickPlayer)
+        }
+      })
+    } else if (action.name === 'killer') {
+      console.log('Kill by ', ip)
+      fetch('http://localhost:8080/game/kill/' + ip).then(res => {
+        console.log(res.status)
+        console.log(res)
+      })
+    } else if (action.name === 'killed') {
+      console.log('Was killed: ', ip)
+      fetch('http://localhost:8080/game/killed/' + ip).then(res => {
+        console.log(res.status)
+        console.log(res)
+        if (res.status !== 200) {
+          console.log('kick player for having no balance')
+          const kickPlayer = 'kick ' + action.index + '\n'
+          quake.stdin.write(kickPlayer)
+        }
+      })
     }
   }
+
+  prevToken = token
 })
