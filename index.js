a79b9226e55f8c3390106458263d423fee88bc23
njlie <lie4nathan@gmail.com>
2018-05-03 17:57:39 -0700

    feat: securing gameplay

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const fetch = require('node-fetch')
 const config = require('./config.json')
 var StreamSplitter = require('stream-splitter')
 const WebSocket = require('ws')
-const quake = spawn('node', ['build/ioq3ded.js', '+set', 'fs_game', 'baseq3', '+set', 'dedicated', '2', '+exec', 'server.cfg'])
+const jwt = require('jsonwebtoken')
+const quake = spawn('node', ['build/ioq3ded.js', '+set', 'fs_game', 'baseq3', '+set', 'dedicated', '1', '+exec', 'server.cfg'])
 var cmdArray = []
 var prevToken = ''
 var playerList = {}
 
 const ws = new WebSocket(`ws://${config.baseUrl}/server`)
+
+function makeJWT () {
+  const payload = {
+    'source': 'ILDM'
+  }
+
+  const token = jwt.sign(payload, config.secret, {
+    algorithm: 'HS256',
+    expiresIn: 3600,
+    issuer: 'ILDeathmatch'
+  })
+
+  return token
+}
 
 ws.on('open', function open () {
   ws.send('server ws connected.')
@@ -16,24 +31,51 @@ ws.on('open', function open () {
 
 ws.on('message', (message) => {
   console.log(message)
+  const parse = JSON.parse(message)
+  const msg = parse.msg || ''
+  if (msg.includes('--ILDM_CONNECT_CLIENT')) {
+    playerList[parse.index] = parse.id
+    fetch(`http://${config.baseUrl}/game/spawn/` + parse.id, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ jwt: makeJWT() })
+    }).then(res => {
+      console.log(res.status)
+      if (res.status !== 200) {
+        console.log('kick player for having no balance/pointer')
+        const kickPlayer = 'kick ' + parse.index + '\n'
+        quake.stdin.write(kickPlayer)
+      }
+    })
+  }
 })
 
-var test = quake.stderr.pipe(StreamSplitter('\n'))
-test.encoding = 'utf-8'
-test.on('token', token => {
+var quakeErr = quake.stderr.pipe(StreamSplitter('\n'))
+quakeErr.encoding = 'utf-8'
+quakeErr.on('token', token => {
   console.log('TOKEN: ', token)
 
   if (token.includes('ClientBegin:')) {
     const client = token.split(':').map(e => e.trim())[1]
     const dumpuser = 'dumpuser ' + client + '\n'
-    cmdArray.push({ name: 'ip', index: client })
-    quake.stdin.write(dumpuser)
+    const telluser = 'tell ' + client + ' --ILDM_CONNECT ' + client + '\n'
+    // cmdArray.push({ name: 'ip', index: client })
+    // quake.stdin.write(dumpuser)
+    quake.stdin.write(telluser)
   }
 
   if (token.includes('ClientDisconnect:')) {
     const client = token.split(':').map(e => e.trim())[1]
     console.log('Disconnected: ', playerList[client])
-    fetch(`http://${config.baseUrl}/game/disconnect/` + playerList[client])
+    fetch(`http://${config.baseUrl}/game/disconnect/` + playerList[client], {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({jwt: makeJWT()})
+    })
     delete playerList[client]
   }
 
@@ -42,14 +84,40 @@ test.on('token', token => {
     const killer = players[1]
     const killed = players[2]
     console.log(`Player ${killer} killed Player ${killed}`)
-    if (killer !== '1022' || killer !== killed) {
-      cmdArray.push({ name: 'killer' })
+    if (killer !== '1022' && killer !== killed) {
+      // cmdArray.push({ name: 'killer' })
+      fetch(`http://${config.baseUrl}/game/kill/` + playerList[killer], {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({jwt: makeJWT()})
+      }).then(res => {
+        console.log(res.status)
+        console.log(res)
+      })
     }
-    cmdArray.push({ name: 'killed', index: killed })
-    const dumpKiller = 'dumpuser ' + killer + '\n'
-    const dumpKilled = 'dumpuser ' + killed + '\n'
-    quake.stdin.write(dumpKiller)
-    quake.stdin.write(dumpKilled)
+    // cmdArray.push({ name: 'killed', index: killed })
+    // const dumpKiller = 'dumpuser ' + killer + '\n'
+    // const dumpKilled = 'dumpuser ' + killed + '\n'
+    // quake.stdin.write(dumpKiller)
+    // quake.stdin.write(dumpKilled)
+
+    fetch(`http://${config.baseUrl}/game/killed/` + playerList[killed], {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({jwt: makeJWT()})
+    }).then(res => {
+      console.log(res.status)
+      console.log(res)
+      if (res.status !== 200) {
+        console.log('kick player for having no balance/pointer')
+        const kickPlayer = 'kick ' + killed + '\n'
+        quake.stdin.write(kickPlayer)
+      }
+    })
   }
 
   if (token.includes('ip        ') && prevToken === '--------') {
@@ -59,9 +127,14 @@ test.on('token', token => {
     console.log('ACTION: ', action)
     if (action.name === 'ip') {
       console.log('Got IP: ', ip)
-      playerList[action.index] = ip
-      fetch(`http://${config.baseUrl}/game/spawn/` + ip).then(res => {
-        console.log(res.body)
+      // playerList[action.index] = ip
+      fetch(`http://${config.baseUrl}/game/spawn/` + ip, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({jwt: makeJWT()})
+      }).then(res => {
         console.log(res.status)
         if (res.status !== 200) {
           console.log('kick player for having no balance/pointer')
